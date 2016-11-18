c8beb89cfd8828c5587aae257dbd4f0ab5edf168
Aaron Young <m_aaron@yahoo.com>
2016-11-18 17:29:17 -0600

    trying to get integrated with M<'s, 'e, 'c>

--- ProceduralStateMachine/NoAsync.fsx
+++ ProceduralStateMachine/NoAsync.fsx
@@ -95,21 +95,43 @@ module Proc =
     let getState: P<'s, 's> = 
         (Ready, fun s -> Done (s, s))
 
+    // A procedure that waits for the next state that matches the predicate.  It does *not* 
+    // consider the current state, and always waits for the next one.
+    let rec waitForNextState predicate =
+        let f s = 
+            match predicate s with
+            | true -> Done (s, s)
+            | false -> Next (s, waitForNextState predicate)
+        (Waiting, f)
+
     // A procedure that returns the state once the given predicate returns true.  If the current 
     // state matches, it completes immediately without waiting for the next state.
     let waitForState predicate =
-        let rec waitNextState =
-            let f s = 
-                match predicate s with
-                | true -> Done (s, s)
-                | false -> Next (s, waitNextState)
-            (Waiting, f)
-
         let checkCurrentState s =
             if predicate s then Done (s, s)
-            else Next (s, waitNextState)
+            else Next (s, waitForNextState predicate)
         
         (Ready, checkCurrentState)
+    
+    let rec onNextState predicate =
+        let f s =
+            match predicate s with
+            | Some pnext -> Next (s, pnext)
+            | None -> Next (s, onNextState predicate)
+
+        (Waiting, f)
+
+    let rec onState predicate =
+        let f s =
+            match predicate s with
+            | Some pnext -> Next (s, pnext)
+            | None -> Next (s, onNextState predicate)
+
+        (Ready, f)
+
+    let setState f =
+        let cont s = Done (f s, s)
+        (Ready, cont)
     
     let proc = PBuilder()
 
@@ -162,60 +184,6 @@ let events = seq { for i in 0 .. 20 do yield i.ToString() }
 
 let result = run events p3
 
-// This is rather surprising (to me, at least), but the bool flag is needed here in order to know 
-// whether the Co is created with Bind or Return.  If created with Return, it implies we aren't 
-// waiting for further input.  It seems like this is redundant with the "Done/Next" cases in the 
-// CoResult type, but it's not the same thing, apparently.
-type Co<'s, 'r> = bool * ('s -> CoResult<'s, 'r>)
-and CoResult<'s, 'r> = 
-    | Done of 'r
-    | Next of Co<'s, 'r>
-
-type CoBuilder() =
-    member x.Bind((b, co), f) =
-        true, fun s ->
-            match co s with
-            | Done r -> Next (f r)
-            | Next (b, n) -> Next (x.Bind((b, n), f))
-
-    member x.Return(v): Co<'s, 'r> =
-        false, (fun s -> Done v)
-
-let co = CoBuilder()
-
-let rec ifOdd s =
-    match s % 2 = 0 with
-    | true -> Done s
-    | false -> Next (true, ifOdd)
-
-let ifOddC = (true, ifOdd)
-
-let cor = co { 
-    let! fo = ifOddC
-    return fo 
-}
-
-let co1 = co {
-    let! isOdd = ifOddC
-    let! isOdd2 = ifOddC
-    let! v3 = cor
-    return isOdd, isOdd2, v3
-}
-
-let co2 = 
-    co.Bind(
-        ifOddC, (fun isOdd -> 
-            co.Bind(
-                ifOddC, (fun isOdd2 -> 
-                    co.Return(isOdd, isOdd2)))))
-
-let step s next = 
-    match next with
-    | Done r -> Done r
-    | Next (b, n) -> n s
-
-(snd co1) 2 |> step 6 |> step 5
-
 type C<'c> = | List of 'c list
 
 module Cmd =
@@ -227,6 +195,8 @@ module Cmd =
 
     let add cmd (List l) = List (cmd :: l)
 
+    let batch cmds = List [ for (List l) in cmds do for c in l do yield c ]
+
 type Event =
     | Type1 of int
     | Type2 of string
@@ -236,136 +206,100 @@ type M<'s, 'e, 'c> = {
     update: 'e -> 's -> 's * C<'c>
 }
 
-module Proc =
-    type TimerId = int
+type TimerId = int
 
-    type ProcedureEvent<'e> =
-        | Cancel
-        | Timeout of TimerId
-        | MachineEvent of 'e
+type ProcedureEvent<'e> =
+    | Cancel
+    | Timeout of TimerId
+    | MachineEvent of 'e
 
-    type ProcedureCommand<'c> =
-        | StartTimer of TimerId * System.TimeSpan
-        | Automation of string * ((string * string) list)
-        | MachineCommand of 'c
+type ProcedureCommand<'c> =
+    | StartTimer of TimerId * System.TimeSpan
+    | Automation of string * ((string * string) list)
+    | MachineCommand of 'c
 
-    type Context<'s, 'e, 'c> = {
-        nextTimerId: TimerId
-        waitTimer: TimerId
-        machine: M<'s, 'e, 'c>
-        state: 's
-        command: C<ProcedureCommand<'c>>
-        event: 'e
-        processor: C<ProcedureCommand<'c>> -> unit
+type TestState<'s, 'e, 'c> = {
+    state: 's
+    event: ProcedureEvent<'e>
+    command: C<ProcedureCommand<'c>>
+    machine: M<'s, 'e, 'c>
+    nextTimerId: TimerId
+    waitTimer: TimerId
     }
 
-    type Result<'a> =
-        | Cancelled
-        | Timedout of TimerId
-        | Exception of System.Exception
-        | Completed of 'a
+type TestProc<'s, 'e, 'c, 'r> = Proc.P<TestState<'s, 'e, 'c>, 'r>
 
-    type P<'s, 'e, 'c, 'a> = 
-        //P of (Context<'s, 'e, 'c> -> Async<Context<'s, 'e, 'c> * Result<'a>>)
-        P of ('e -> 's -> 's * C<ProcedureCommand<'c>> * Step<'s, 'e, 'c, 'a>)
+let updateContext predicate context procEvent =
+    match procEvent with
+    | Cancel -> context, Some Cancelled
+    | Timeout id -> 
+        context, 
+        // Ignore timer expirations for procedures that have already completed
+        if id = context.waitTimer then Some (Timedout id) 
+        else None
+    | MachineEvent e -> 
+        match predicate e with
+        | None -> 
+            let (newState, newCommand) = context.machine.update e context.state
+            { context with state = newState; command = Cmd.map MachineCommand newCommand }, None
+        | Some a -> context, Some (Completed a)
 
-    and Step<'s, 'e, 'c, 'a> =
-        | Done of 'a
-        | Next of P<'s, 'e, 'c, 'a>
+let contextStartTimer duration context =
+    { context with 
+        nextTimerId = context.nextTimerId + 1
+        command = Cmd.add (ProcedureCommand.StartTimer (context.nextTimerId, duration)) context.command }
 
-    let retn a =
-        P (fun e s -> s, Cmd.none, Done a)
+let contextProcessCommand context =
+    context.processor context.command
+    { context with command = Cmd.none }
 
-    let bind f (P p) =
-        let run c = 
-            async {
-                let! cnew, a = p c
-                return! 
-                    match a with
-                    | Cancelled -> async { return c, Cancelled }
-                    | Timedout id -> async { return c, Timedout id }
-                    | Exception ex -> async { return c, Exception ex }
-                    | Completed a -> 
-                        let (P p2) = f a
-                        p2 cnew
-            }
-        P run
+let startWaitTimer duration = proc {
+    let! s = setState (fun s -> 
+        { s with 
+            command = 
+                Cmd.add (StartTimer duration) s.command 
+        })
+    return s
+    }
 
-    let updateContext predicate context procEvent =
-        match procEvent with
-        | Cancel -> context, Some Cancelled
-        | Timeout id -> 
-            context, 
-            // Ignore timer expirations for procedures that have already completed
-            if id = context.waitTimer then Some (Timedout id) 
-            else None
-        | MachineEvent e -> 
-            match predicate e with
-            | None -> 
-                let (newState, newCommand) = context.machine.update e context.state
-                { context with state = newState; command = Cmd.map MachineCommand newCommand }, None
-            | Some a -> context, Some (Completed a)
+let defaultUpdate event testState =
+    let (newState, newCmd) = testState.machine.update event testState.state
+    { testState with
+        state = newState
+        command = Cmd.batch [(Cmd.map MachineCommand newCmd); testState.command]
+    }
 
-    let contextStartTimer duration context =
-        { context with 
-            nextTimerId = context.nextTimerId + 1
-            command = Cmd.add (ProcedureCommand.StartTimer (context.nextTimerId, duration)) context.command }
+let waitForEvent duration predicate: TestProc<'state, 'event, 'command, 'event option> =
+    proc {
+        let! timerId = startWaitTimer duration
+        let! s = Proc.onNextState (fun s -> 
+            match s.event with
+            | Timeout timerId -> proc { return None } |> Some
+            | MachineEvent e ->
+                if predicate e then proc { return Some e } |> Some
+                else proc {
+                    let! s1 = Proc.setState (defaultUpdate e)
+                    return None 
+                } |> Some
+            | _ -> None)
 
-    let contextProcessCommand context =
-        context.processor context.command
-        { context with command = Cmd.none }
+        return s
+    }
 
-    let waitForEvent duration predicate: P<'state, 'event, 'command, 'result> =
-        let run context =
-            async {
-                let mutable ctx = contextStartTimer duration context
-                let mutable result = None
-                while result.IsNone do
-                    ctx <- contextProcessCommand ctx
-                    let! event = context.events ()
-                    let (newCtx, newResult) = updateContext predicate ctx event
-                    ctx <- newCtx
-                    result <- newResult
-                return ctx, result.Value
-            }
-        P run
-
-    let onEvent duration selectCont: P<'state, 'event, 'command, 'result> =
-        let run context =
-            async {
-                let mutable ctx = contextStartTimer duration context
-                let mutable result = None
-                while result.IsNone do
-                    ctx <- contextProcessCommand ctx
-                    let! event = context.events ()
-                    let (newCtx, newResult) = updateContext selectCont ctx event
-                    ctx <- newCtx
-                    result <- newResult
-                return ctx, result.Value
-            }
-        bind id (P run)
-
-    let runProc (P p) m events processor =
-        let (s, c) = m.init
+let onEvent duration selectCont: P<'state, 'event, 'command, 'result> =
+    let run context =
         async {
-            let context = { 
-                state = s
-                command = Cmd.map MachineCommand c
-                machine = m
-                events = events
-                processor = processor
-                nextTimerId = 0
-                waitTimer = 0 
-            }
-            let! result = p context
-            return snd result
+            let mutable ctx = contextStartTimer duration context
+            let mutable result = None
+            while result.IsNone do
+                ctx <- contextProcessCommand ctx
+                let! event = context.events ()
+                let (newCtx, newResult) = updateContext selectCont ctx event
+                ctx <- newCtx
+                result <- newResult
+            return ctx, result.Value
         }
-
-type ProcedureBuilder() =
-    member x.Bind(p, f) = Proc.bind f p
-    member x.Return(v) = Proc.retn v
-
-let proc = new ProcedureBuilder()
+    bind id (P run)
 
 type Command = string
 
