f85ff1dd568bca121386bc6941a3b675086177e3
maaron <m_aaron@yahoo.com>
2016-11-22 23:45:01 -0600

    some refactoring and cleanup

--- ProceduralStateMachine/NoAsync.fsx
+++ ProceduralStateMachine/NoAsync.fsx
@@ -21,23 +21,28 @@ module Seq =
 module Proc =
     type Wait = Waiting | Ready
 
-    type P<'s, 'a> = Wait * ('s -> R<'s, 'a>)
+    type P<'s, 'a> = P of Wait * ('s -> R<'s, 'a>)
 
     and R<'s, 'a> = 
         | Done of 's * 'a
         | Next of 's * P<'s, 'a>
 
-    type PBuilder() =
-        member x.Bind(p: P<'s, 'a>, f: 'a -> P<'s, 'a2>): P<'s, 'a2> =
-            let (wait, cont) = p
-            let step state =
-                match cont state with
-                | Done (s, a) -> Next (s, f a)
-                | Next (s, pnext) -> Next (s, x.Bind(pnext, f))
-            (wait, step)
+    let rec bind f p =
+        let (P (wait, cont)) = p
+        let step state =
+            match cont state with
+            | Done (s, a) -> Next (s, f a)
+            | Next (s, pnext) -> Next (s, bind f pnext)
+        P (wait, step)
 
-        member x.Return(v: 'a): P<'s, 'a> = 
-            (Ready, fun s -> Done (s, v))
+    let (>>=) p f = bind f p
+
+    let returnProc v = P (Ready, fun s -> Done (s, v))
+
+    type PBuilder() =
+        member x.Bind(p: P<'s, 'a>, f: 'a -> P<'s, 'a2>): P<'s, 'a2> = bind f p
+
+        member x.Return(v: 'a): P<'s, 'a> = returnProc v
 
         member x.ReturnFrom(p) = p
 
@@ -45,97 +50,59 @@ module Proc =
     // or waiting for the next state
     let rec readyStep (r: R<'s, 'a>): R<'s, 'a> =
         match r with
-        | Next (sold, (Ready, cont)) -> readyStep (cont sold)
+        | Next (sold, P (Ready, cont)) -> readyStep (cont sold)
         | _ -> r
     
     // Starts a procedure by returning the first result
     let start p s = 
         let firstResult = 
             match p with
-            | (Ready, cont) -> cont s
-            | (Waiting, cont) -> Next (s, p)
+            | P (Ready, cont) -> cont s
+            | P (Waiting, cont) -> Next (s, p)
         readyStep firstResult
 
     // Steps a procedure along by returning the next result.  It is recursive, as internally it 
     // actually steps until the procedure indicates it is waiting for the next input
     let step (s: 's) (r: R<'s, 'a>): R<'s, 'a> =
-        
         // Unless we're already done, get the next result and immediately process any "ready" 
         // procedures
         match r with
-        | Next (_, (_, cont)) -> cont s |> readyStep 
+        | Next (_, P (_, cont)) -> cont s |> readyStep 
         | _ -> r
+
+    // Applies a sequence of events to a result, stopping early if the procedure completes.
+    let stepAll events result =
+        let next result event = step event result
+
+        let isDone = function
+            | Done _ -> true
+            | _ -> false
+
+        Seq.scan next result events
+        |> Seq.takeUntil isDone
 
     // Runs a procedure by stepping it for all the events in the sequence.  Note that the 
     // procedure may not yet be complete when the function returns, as it may still be expecting 
     // further events.  In the event that the procedure completes before all the events are 
-    // consumed, the function returns early.  The event sequence must be non-empty, or else the 
-    // function returns None.
-    let run events proc =
-        let init = None
-        
-        let next resultOpt event = 
-            let result =
-                match resultOpt with
-                | None -> Some (start proc event)
-                | Some r -> Some (step event r)
-            result
-        
-        let isDone result = 
-            match result with
-            | Some (Done _) -> true
-            | _ -> false
+    // consumed, the function returns early.
+    let run event events proc =
+        start proc event |> stepAll events
 
-        Seq.scan next init events
-        |> Seq.takeUntil isDone
-        |> Seq.choose id
-        |> Seq.tryLast
+    let getState2 f =
+        P (Ready, fun s -> Done (s, f s))
 
     // A procedure that just returns the current state
     let getState: P<'s, 's> = 
-        (Ready, fun s -> Done (s, s))
+        P (Ready, fun s -> Done (s, s))
 
     // A procedure that waits for and returns the next state
     let getNextState: P<'s, 's> =
-        (Waiting, fun s -> Done (s, s))
-
-    // A procedure that waits for the next state that matches the predicate.  It does *not* 
-    // consider the current state, and always waits for the next one.
-    let rec waitForNextState predicate =
-        let f s = 
-            match predicate s with
-            | true -> Done (s, s)
-            | false -> Next (s, waitForNextState predicate)
-        (Waiting, f)
-
-    // A procedure that returns the state once the given predicate returns true.  If the current 
-    // state matches, it completes immediately without waiting for the next state.
-    let waitForState predicate =
-        let checkCurrentState s =
-            if predicate s then Done (s, s)
-            else Next (s, waitForNextState predicate)
-        
-        (Ready, checkCurrentState)
+        P (Waiting, fun s -> Done (s, s))
     
-    let rec onNextState predicate =
-        let f s =
-            match predicate s with
-            | Some pnext -> Next (s, pnext)
-            | None -> Next (s, onNextState predicate)
-
-        (Waiting, f)
-
-    let rec onState predicate =
-        let f s =
-            match predicate s with
-            | Some pnext -> Next (s, pnext)
-            | None -> Next (s, onNextState predicate)
-
-        (Ready, f)
-
+    // A procedure that applies a function to modify the state
     let setState f =
-        let cont s = Done (f s, s)
-        (Ready, cont)
+        let cont s = let snew = f s in Done (snew, snew)
+        P (Ready, cont)
     
     let proc = PBuilder()
 
@@ -162,7 +129,7 @@ let p3 = proc {
             let! a2 = p1 2
             return a1, a2
         }
-    let! s2 = waitForState (fun (s: string) -> s.Length > 1)
+    let! s2 = getNextState
     let! a3 = p1 3
     let! s3 = getNextState
     return a1, a2, a3, s, s2, s3
@@ -184,9 +151,9 @@ start p3 "0" |> step "1" |> step "2" |> step "3" |> step "4" |> step "50" |> ste
 
 start (proc { return! getState }) "asdf" |> step "qwer"
 
-let events = seq { for i in 0 .. 20 do yield i.ToString() }
+let (event, events) = "0", [ for i in 1 .. 20 do yield i.ToString() ]
 
-let result = run events p3
+let result = run event events p3
 
 type C<'c> = | List of 'c list
 
@@ -232,7 +199,6 @@ type TestState<'s, 'e, 'c> = {
 type TestProc<'s, 'e, 'c, 'r> = Proc.P<TestState<'s, 'e, 'c>, 'r>
 
 let startWaitTimer duration = 
-    printf "startWaitTimer\n"
     let update state =
         { state with 
             nextTimerId = state.nextTimerId + 1
@@ -252,8 +218,6 @@ let defaultUpdate event testState =
         command = Cmd.batch [(Cmd.map MachineCommand newCmd); testState.command]
     }
 
-
-
 let tryWaitForEvent duration predicate: TestProc<'state, 'event, 'command, 'result option> =
     let rec processEvent predicate timerId = 
         proc {
@@ -263,11 +227,13 @@ let tryWaitForEvent duration predicate: TestProc<'state, 'event, 'command, 'resu
             | TimerExpired id when id = timerId -> 
                 return None
 
-            // Predicate matches, give event to proc
             | MachineEvent e ->
                 match predicate e with
-                | Some r -> return Some r
+                | Some r -> 
+                    // Predicate matches, give event to proc
+                    return Some r
                 | None -> 
+                    // Predicate doesn't match, use default update
                     let! s1 = Proc.setState (defaultUpdate e)
                     return! processEvent predicate timerId
 
@@ -276,9 +242,7 @@ let tryWaitForEvent duration predicate: TestProc<'state, 'event, 'command, 'resu
         }
 
     proc {
-        printf "starting timer\n"
         let! timerId = startWaitTimer duration
-        printf "timer started %A\n" timerId
         return! processEvent predicate timerId
     }
 
@@ -291,13 +255,16 @@ let tryOnEvent duration selectCont: TestProc<'state, 'event, 'command, 'result o
             | TimerExpired id when id = timerId -> 
                 return None
 
-            // Predicate matches, give event to proc
             | MachineEvent e ->
                 match selectCont e with
                 | Some pnext -> 
+                    // Predicate matches, run next procedure
                     let! result = pnext
                     return Some result
-                | None -> return! processEvent selectCont timerId
+                | None -> 
+                    // Predicate doesn't match, apply default update
+                    let! s1 = Proc.setState (defaultUpdate e)
+                    return! processEvent selectCont timerId
 
             // Non-matching timer- keep waiting
             | _ -> return! processEvent selectCont timerId
@@ -379,4 +346,3 @@ startTc machine tc2 |> processResult
 |> stepTc (MachineEvent (Type1 2345)) |> processResult
 |> stepTc (MachineEvent (Type2 "12345")) |> processResult 
 |> stepTc (MachineEvent (Type2 "123 #2")) |> processResult 
-
